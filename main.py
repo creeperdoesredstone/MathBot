448af66c0ad0e5f90bb08caa692221648f23f6ba
creeperDoesRedstone <142895378+creeperDoesRedstone@users.noreply.github.com>
2023-08-22 17:56:53 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+# Import required modules
+import nextcord
+import os
+from nextcord.ext import commands
+from keep_up import keep_awake
+
+# Version
+version = 'MathBot | v 3.2'
+
+# Discord stuff
+intents = nextcord.Intents().default()
+intents.message_content = True
+client = commands.Bot(command_prefix='/', intents=intents)
+
+# First event: logging in
+@client.event
+async def on_ready():
+	await client.change_presence(status=nextcord.Status.idle,
+	                             activity=nextcord.Game(' around with math | ' + version[10:len(version)]))
+	print("{0.user} is now online.".format(client))
+
+initial_extensions = []
+
+for fn in os.listdir('./cogs'):
+	if fn.endswith('.py'):
+		initial_extensions.append('cogs.' + fn[:-3])
+
+print(initial_extensions)
+
+if __name__ == '__main__':
+	for e in initial_extensions:
+		client.load_extension(e)
+
+# Calling the awake function
+keep_awake()
+# Getting the secret token
+client.run(os.getenv('TOKEN'))
